859d3ba79eff08c91d8eb61a798c1ab65c59502f
ak375456 <ak375456@gmail.com>
2025-01-07 18:04:16 +0500

    Added comments

--- app/src/main/java/com/example/socialconnect/home/presentation/ViewProfileOfUsers.kt
+++ app/src/main/java/com/example/socialconnect/home/presentation/ViewProfileOfUsers.kt
@@ -11,8 +11,11 @@ import com.example.socialconnect.post_feature.presentation.User
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.setValue
 
+//Fix Ui tomorrow for profile Screen
+
 @Composable
 fun ViewProfile(userId: String, postViewModel: PostViewModel = hiltViewModel()) {
+    // this User is not from Firebase Authentication, it is a repository.
     var user by remember { mutableStateOf<User?>(null) }
 
     LaunchedEffect(userId) {
